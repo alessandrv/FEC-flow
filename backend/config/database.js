0175f3376fa6538a5ea30a779a35d2e6eb69b379
Alessandro Piccin <alessandropiccin00@gmail.com>
2025-09-18 10:17:56 +0200

    Ip update

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const dbConfig = {
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'Fecitalia2024',
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME || 'flow_platform',
   port: process.env.DB_PORT || 3306,
   waitForConnections: true,
